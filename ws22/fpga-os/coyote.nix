bca63a3bdc7cdc759ae13fd9556e3503096612e4
Jörg Thalheim <joerg@thalheim.io>
2023-03-08 18:38:26 +0000

    drop second shell by inlining xilinx-shell into coyote one
    This makes sure that both are providing the same libc version.
    Also the project patch is no longer needed.

--- ws22/fpga-os/coyote.nix
+++ ws22/fpga-os/coyote.nix
@@ -1,37 +1,69 @@
-with import <nixpkgs> {}; 
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1e383aada51b416c6c27d4884d2e258df201bc11.tar.gz") {}
+}:
 
-let 
-  my-python = pkgs.python3;
-  # jinja2 is required to build Coyote. 
-  python-with-my-packages = my-python.withPackages (p: with p; 
-  [
-    jinja2
-  ]);
+with pkgs;
+let
+  python-with-jinja2 = python3.withPackages (p: with p; [ jinja2 ]);
 in
-pkgs.mkShell {
-  name = "my-packages";
-  runScript = "bash";
-
-  buildInputs = with pkgs; [
+(buildFHSUserEnv {
+  name = "coyote-shell";
+  targetPkgs = pkgs: with pkgs; [
+    bash
     coreutils
-    git 
+    zlib
+    lsb-release
+    stdenv.cc.cc
+    ncurses5
+    xorg.libXext
+    xorg.libX11
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libXi
+    xorg.libXft
+    xorg.libxcb
+    xorg.libxcb
+    # common requirements
+    freetype
+    fontconfig
+    glib
+    libxcrypt
+    gtk2
+    gtk3
+    (libidn.overrideAttrs (old: {
+      # we need libidn.so.11 but nixpkgs has libidn.so.12
+      src = fetchurl {
+        url = "mirror://gnu/libidn/libidn-1.34.tar.gz";
+        sha256 = "sha256-Nxnil18vsoYF3zR5w4CvLPSrTpGeFQZSfkx2cK//bjw=";
+      };
+    }))
+
+    # to compile some xilinx examples
+    opencl-clhpp
+    ocl-icd
+    opencl-headers
+
+    (pkgs.runCommand "firmware" { } ''
+      mkdir -p $out/lib/firmware
+      ln -s /host/lib/firmware/xilinx $out/lib/firmware/xilinx
+    '')
+
+    # from installLibs.sh
+    graphviz
+    (lib.hiPrio gcc)
+    unzip
+    nettools
+
+    git
     boost
 
-    cmake
     nasm
     binutils
-
-    # linux.dev
     usbutils
     pciutils
-
-    # python packages
-    python3
-    python-with-my-packages
-  ];  
-  shellHook = ''
-    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
-    # the other env-vars
+    python-with-jinja2
+  ];
+  multiPkgs = null;
+  profile = ''
+    source /share/xilinx/Vitis/2021.2/settings64.sh
   '';
-}
-
+}).env
